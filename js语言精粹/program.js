85398e89d98d98fa08a3fbeafd9a2d0f8b2e9eb8
AngellinaZ <AngellinaZ_zyp@163.com>
2017-07-26 09:29:05 +0800

    Update program.js

--- js语言精粹/program.js
+++ js语言精粹/program.js
@@ -146,7 +146,7 @@ var add = function (a, b){
 
 //3.方法调用模式
 	//当一个函数被保存为对象的一个属性时，我们称他为一个方法  xxx.xx()
-	//方法可以使用this来访问自己所属的对象；this到对象的绑定发生在调用时----公共方法
+	//this绑定：当一个方法被调用时，this被绑定到该对象。this到对象的绑定发生在调用时----公共方法
 var myObject = {
 	value: 0,
 	increment: function (inc) {
@@ -170,14 +170,13 @@ myObject.double = function () {
 	}
 	helper();  //以函数的形式调用helper
 }
-    //以方法的形式调用double
-myObject.double();
+myObject.double(); //以方法的形式调用double
 document.write(myObject.value)  //6  
 
 //5.构造器调用模式
-//JS是一门基于原型继承的语言，意味着对象可以直接从其他对象继承属性。该语言是无类型的
-//如果在一个函数前面带上new来调用，那么背地里将会创建一个 连接到该函数的prototype成员的 新对象，同时this会被绑定到那个新对象上
+//JS是一门基于  原型继承 的语言，意味着对象可以直接从其他对象继承属性。该语言是无类型的
 //构造器函数：一个函数，创建的目的就是希望结合new前缀来调用    
+//new 函数(): 背地里将会创建一个 连接到该函数的prototype成员的 新对象，同时this会被绑定到那个新对象上
 //this绑定：指向构造器函数的原型
 	//创建一个名为Quo的构造器函数，它构造一个带有status属性的对象
 var Quo = function (string) {
@@ -190,24 +189,28 @@ Quo.prototype.get_status = function () {
 	//构造一个Quo实例 调用形式：在函数名前加 new 来调用,new方法返回的是构造器函数原型的引用
 var myQuo = new Quo("confused"); //myQuo连接到的是Quo的原型
 console.log(myQuo.get_status()); //confused  this.status即Quo.prototype.status在调用new构造Quo时即被赋予了confused。
+console.log(myQuo.status); //confused 
+typeof Quo; //function 
+typeof MyQuo; //Object
 
-	//？？？问：为什么访问不到Quo.status
+	//？？？问：为什么访问不到Quo.status 
 
 //6.Apply调用模式
 	//apply方法接收两个参数，第1个是要绑定给this的值，第2个就是一个参数数组
 var array = [3, 4];
-var sum = add.apply(null, array);
-console.log('sum = ' + sum);
+var sum = add.apply(null, array);//sum = 7
 
 var statusObject = {
 	status: 'A-OK'
 }
-	//statusObject并没有继承自Quo.prototype,但我们可以在statusObject上调用get_status方法，
-	//尽管statusObject并没有一个名为get_status的方法。
+	//statusObject并没有继承Quo.prototype,但可以在statusObject上调用get_status方法，尽管statusObject并没有一个名为get_status的方法。
 var status = Quo.prototype.get_status.apply(statusObject); //status值为'A-OK'
 
 //7.参数
-	//当函数被调用时，会得到一个参数---arguments数组, arguments是一个‘类似数组’对象。拥有一个length属性，没有其他数组的方法
+//当函数被调用时，会得到一个参数---arguments数组, arguments是一个‘类似数组’对象。拥有一个length属性，没有其他数组的方法
+//函数可以通过此arguments访问所有它被调用时 递给它的参数列表，包括没有被分配给函数声明时定义的形式参数的多余参数
+	//注意该函数内部定义的变量sum不会与函数外部定义的sum产生冲突
+	//该函数只会看到内部的那个变量
 var sum = funciton () {
 	var sum = 0;
 	for (var i = 0; i < arguments.length; i++) {
