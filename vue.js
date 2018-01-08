ed57d71ef15060392c03c3d6edc18a7be422e4f9
AngellinaZ <AngellinaZ_zyp@163.com>
2018-01-08 09:13:55 +0800

    Update vue.js

--- vue.js
+++ vue.js
@@ -1,6 +1,6 @@
 1. "如何在v-for循环中使用v-model" 
     v-for循环对象要求是Array, 通过index给Array内的对象添加属性 eg: arr[index].attrubute
-    注意： 建议在使用v-for时添加 :key <li v-for="item in items" :key="item.id"></li>
+    注意： 建议在使用v-for时添加 :key <li v-for="item in items" :key="item.id"></li> 不同浏览器，加载能力不同
     
 2."vue的form提交"
     通过定义formDate对象提交
